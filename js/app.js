b16e00060e5d79891b811d28aa9525bc7a02408a
davidvdev <dvsummercamp@gmail.com>
2021-07-12 14:23:31 -0500

    API working

--- js/app.js
+++ js/app.js
@@ -13,3 +13,30 @@ const toggleNav = $(`.fa-bars`).on(`click`, () => {
     }
 })
 
+
+// Google Sheet API Call using jQuery
+// $.ajax("https://docs.google.com/spreadsheets/d/18t7o7jtwXsDRKUEUge9CFPbaVwezAvDzNQxUNaOM590/edit#gid=0")
+// .then((data) => {
+//     console.log(data);
+// })
+
+$.ajax("./json/projects.json")
+.then((data) => {
+    data.forEach((project, index) => {
+        const $container = $('.work-card-gallery')
+        const $card = $('<div class="work-card">')
+        $card.html(`
+        <img src="${project.image}" alt="a photo of the ${project.name} project" />
+        <div class="card-text">
+            <h3>${project.name}</h3>
+            <p>${project.description}</p>
+        </div>
+        <div class="card-button-group">
+            <a class="card-button card-code">Code</a>
+            <a class="card-button card-live">Live</a>
+            <a class="card-button card-ask">Ask Me</a>
+        </div>
+        `)
+        $container.append($card)
+    })
+})
