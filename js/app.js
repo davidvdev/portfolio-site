17050db94273ebdef768044ca12a7ae0b6b7e633
davidvdev <dvsummercamp@gmail.com>
2021-07-12 16:00:53 -0500

    from working with formspree

--- js/app.js
+++ js/app.js
@@ -41,3 +41,25 @@ $.ajax("./json/projects.json")
     })
 })
 .catch((error) => console.log(error))
+
+// Formspree Script
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("contact-me-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    status.innerHTML = "Thanks for your submission!";
+    form.reset()
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
